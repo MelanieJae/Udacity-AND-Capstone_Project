5bf520e5a7a810212bdd49f64ba989eb01bfe65a
Melanie Hand <mjhanddev@gmail.com>
2017-04-20 11:36:03 -0700

    Type of commit: feature
    changes:add widget functionality
    
    resolves issues: n/a

---
+++ app/src/main/java/com/android/melanieh/dignitymemorialandroid/widget/DMWidgetProvider.java
@@ -0,0 +1,41 @@
+package com.android.melanieh.dignitymemorialandroid.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.android.melanieh.dignitymemorialandroid.ui.MenuItemListActivity;
+import com.android.melanieh.dignitymemorialandroid.R;
+
+/*** Created by melanieh on 4/20/17. */
+
+public class DMWidgetProvider extends AppWidgetProvider {
+
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final int N = appWidgetIds.length;
+
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        for (int i=0; i<N; i++) {
+            int appWidgetId = appWidgetIds[i];
+
+            // Create an Intent to launch ExampleActivity
+            Intent intent = new Intent(context, MenuItemListActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            // Get the layout for the App Widget and attach an on-click listener
+            // to the button
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
+            views.setTextViewText(R.id.widget_plan_details, "test plan details");
+            views.setTextViewText(R.id.widget_event_details, "test event details");
+            views.setOnClickPendingIntent(R.id.tappable_widget_layout, pendingIntent);
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+
+}
+
